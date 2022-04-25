4962a934832e23530c1ee66c55011458df0dd574
Rachel Wilson <rachel.wilson@theodi.com>
2022-04-25 15:55:54 +0100

    Add Profile and config for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const axios = require('axios')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const axiosInstance = axios.create({
     baseURL: 'https://openactive.io/data-catalogs'
