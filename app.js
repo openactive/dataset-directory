ecb217366467eb3ed479fdad5224040e9c81f5e6
Howard Askew <19271569+howaskew@users.noreply.github.com>
2023-01-25 16:27:11 +0000

    Brought in RPDE visualiser loop to read from catalogs and parse jsonld

--- app.js
+++ app.js
@@ -1,7 +1,87 @@
 import 'dotenv/config'
 import express from "express";
 import axios from 'axios'
-import {crawl} from './lib/CatalogueCrawler.js'
+import { Handler } from "htmlmetaparser";
+import { Parser } from "htmlparser2";
+
+function extractJSONLDfromHTML(url, html) {
+    let jsonld = null;
+
+    const handler = new Handler(
+        (err, result) => {
+            if (!err && typeof result === 'object') {
+                const jsonldArray = result.jsonld;
+                // Use the first JSON-LD block on the page
+                if (Array.isArray(jsonldArray) && jsonldArray.length > 0) {
+                    [jsonld] = jsonldArray;
+                }
+            }
+        },
+        {
+            url, // The HTML pages URL is used to resolve relative URLs. TODO: Remove this
+        },
+    );
+
+    try {
+        // Create a HTML parser with the handler.
+        const parser = new Parser(handler, {
+            decodeEntities: true,
+        });
+        parser.write(html);
+        parser.end();
+
+        //console.log(jsonld)
+        return jsonld;
+    }
+    catch (error) {
+        console.log("Error parsing dataset site: " + url);
+    }
+
+}
+
+
+async function crawl(dataCatalogUrl) {
+    console.log('1 Calling ' + dataCatalogUrl)
+    try {
+        // Get all datasets on load
+        const collection = await axios.get(dataCatalogUrl);
+
+        if (collection.data && collection.data.hasPart) {
+
+            const datasetUrls = (await Promise.all(collection.data.hasPart.map(async (url) => {
+
+                console.log('2 Calling ' + url)
+                try {
+                    return await axios.get(url, {
+                        timeout: 20000
+                    });
+                }
+                catch (error) {
+                    console.log("Error getting dataset site catalogue: " + url);
+                    return null;
+                }
+            }))).filter(x => x).flatMap(x => x.data.dataset);
+
+            const datasetSites = (await Promise.all(datasetUrls.map(async (url) => {
+                console.log('3 Calling ' + url)
+                try {
+                    return extractJSONLDfromHTML(url, (await axios.get(url, {
+                        timeout: 20000
+                    })).data)
+                }
+                catch (error) {
+                    console.log("Error getting dataset site: " + url);
+                }
+            })))
+
+            return datasetSites
+        }
+    }
+    catch (error) {
+        console.error(error.stack);
+        process.exit(1);
+    }
+}
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,14 +92,12 @@ const axiosInstance = axios.create({
 
 app.get('/datasets', async (req, res) => {
     try {
-        // const response = await axiosInstance.get("https://openactive.io/data-catalogs/data-catalog-collection.jsonld")
         const response = await crawl("https://openactive.io/data-catalogs/data-catalog-collection.jsonld")
         res.status(200).json(response);
     } catch (err) {
         console.log(err);
     }
 })
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
